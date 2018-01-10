9ad345d61b7ed8903b3ae9e40ec408b732fdbe43
stefan86g <35009624+stefan86g@users.noreply.github.com>
2018-01-10 20:49:43 +0200

    add: datapicker, genre selector, validation

--- MovieRental/Models/MovieViewModel.cs
+++ MovieRental/Models/MovieViewModel.cs
@@ -11,11 +11,14 @@ namespace MovieRental.Models
     {
         public int Id { get; set; }
         public string Picture { get; set; }
-        [Required]
+        [Required(ErrorMessage = "A name is required")]
         public string Title { get; set; }
         public DateTime ReleaseDate { get; set; }
+        [Required]
         public string Genre { get; set; }
+        [Required]
         [Range(1,120)]
+        [RegularExpression("([1-9][0-9]*)", ErrorMessage = "Count must be a natural number")]
         public string Price { get; set; }
     }
 }
